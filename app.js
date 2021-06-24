a88d08353cff11b8f6f4a3d457712316136ea0a3
Varun Venkatesh <varunv153@gmail.com>
2021-06-24 17:12:10 +0530

    Database changes
    Created table user and added rows

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 //importing modules
 var express = require('express');
 var bodyParser =require('body-parser');
-const { Sequelize } = require('sequelize');
+//var models = require('./models.js')
+const { Sequelize, DataTypes } = require('sequelize');
+
+
+
 
 //app settings
 var app=express();
@@ -9,13 +13,16 @@ port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+
+
 //connecting database and then testing
 const sequelize = new Sequelize('poly3', 'root', '', {
 	host: 'localhost',
 	port: '3306',
 	dialect: 'mysql'
 });
-async function check_database_working()
+(async()=>
 {
 	try
 	{
@@ -25,7 +32,37 @@ async function check_database_working()
 	catch (error) {
 		console.error('Unable to connect to the database:', error);
 	}
-}
+})();
+
+
+//database model user
+const User = sequelize.define('User', {
+  email: {
+    type: DataTypes.STRING,
+    primaryKey: true,    
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  timestamps: false
+});
+
+console.log( (User === sequelize.models.User)?'User model created':'User model not created' ); // true
+
+(async()=>
+{
+	await User.sync();
+	console.log("User database synced");
+})();
+async function save_row()
+{
+	const jane = User.build({ email: "Varun", password:"Jana"});
+	await jane.save();
+	console.log('Jane was saved to the database!');
+};
+
 
 //routes
 app.post('/signup',(req,res)=>{
@@ -33,5 +70,5 @@ app.post('/signup',(req,res)=>{
 })
 app.listen(port,(req,res)=>{
 	console.log("Listening on port:",port);
-	check_database_working();
+	//check_database_working();
 })
