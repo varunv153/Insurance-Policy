0677b37ba255ced9c1904e1f37fdb3fae26c9eab
Varun Venkatesh <varunv153@gmail.com>
2021-06-25 16:28:01 +0530

    Create Models
    Created All Models without the foregin key

--- Database/Models/models.js
+++ Database/Models/models.js
@@ -5,7 +5,7 @@ const sequelize = require('../db_connection.js');
 const User = sequelize.define('User', {
   email: {
     type: DataTypes.STRING,
-    primaryKey: true,    
+    primaryKey: true,
   },
   password: {
     type: DataTypes.STRING,
@@ -14,10 +14,121 @@ const User = sequelize.define('User', {
   salt:{
   	type: DataTypes.STRING(1000),
     allowNull: false
+  },
+  Name:{
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  PhoneNo:{
+    type: DataTypes.BIGINT(10),
+    allowNull: false
+  },
+}, {
+  timestamps: false
+});
+
+//company user model
+const Company = sequelize.define('Company', {
+  CompanyGSTIN: {
+    type: DataTypes.STRING,
+    primaryKey: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  salt:{
+    type: DataTypes.STRING(1000),
+    allowNull: false
+  },
+  CompanyName:{
+    type: DataTypes.STRING,
+    allowNull: false
   }
 }, {
   timestamps: false
 });
+
+//creating policy model
+const Policy = sequelize.define('Policy', {
+  PolicyID: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,    
+  },
+  PolicyWording: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  RoomRentCap:{
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  SumInsured:{
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  CompanyGSTIN:{
+    type: DataTypes.STRING,
+    allowNull: false,
+    //references: {model: 'asadasCompany',key: 'CsdfompanyGSTIN'}
+  },
+  Exemptions:{
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  ClaimSettlementRatio:{
+    type: DataTypes.DOUBLE,
+    allowNull: false
+  }
+}, {
+  timestamps: false
+});
+
+
+
+
+
+//purchased policies model
+const PurchasedPolicy = sequelize.define('PurchasedPolicy', {
+  BondID: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true    
+  },
+  UserEmail: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    //references: {model: 'User',key: 'email'}
+  },
+  PolicyID:{
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    //references: {model: 'Policy',key: 'PolicyID'}
+  },
+  PolicyHolderName:{
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  PolicyHolderDOB:{
+    type: DataTypes.DATEONLY,
+    allowNull: false
+  },
+  PreExistingDiseases:{
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  Premium:{
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+}, {
+  timestamps: true,
+  updatedAt: false,
+  createdAt: 'PolicyPurchaseDate'
+});
+
+
 
 //check if database is synced
 (async()=>
