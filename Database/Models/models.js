d4504d4b833c1122231e16c327bd503a0afb9e6f
Varun Venkatesh <varunv153@gmail.com>
2021-07-07 13:39:22 +0530

    Routes and model cleaning
    Added some routes and made different columns for primary key and references

--- Database/Models/models.js
+++ Database/Models/models.js
@@ -2,19 +2,21 @@ const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../db_connection.js');
 
 
-function email_column(){ return{ type: DataTypes.STRING, primaryKey: true, validate: {isEmail: true} }};
+function email_pk_column(){ return{ type: DataTypes.STRING, primaryKey: true, validate: {isEmail: true} }};
+function email_column(){ return{ type: DataTypes.STRING, validate: {isEmail: true} }};
 function password_column(){ return{type: DataTypes.STRING, allowNull: false, validate: {len: [8,255]} }};
 function salt_column(){ return{type: DataTypes.STRING(1000), allowNull: false}};
 function name_column(){ return{ type: DataTypes.STRING, allowNull: false, validate: {is: /^([a-z]| |[A-Z])*$/} }};
 function PhoneNo_column(){ return{type: DataTypes.STRING, allowNull: false, validate: {isNumeric: true, len: [10,10], min:0}}};
-function id_column(){ return{ type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true }};
+function id_pk_column(){ return{ type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true }};
+function id_column(){ return{ type: DataTypes.INTEGER}};
 function text_column(){ return{type: DataTypes.TEXT, allowNull: false}};
 function money_column(){ return{ type: DataTypes.INTEGER, allowNull: false, validate: {isNumeric: true, min:1} }};
 
 
 //creating user model
 const user = sequelize.define('user', {
-  email : email_column(),
+  email : email_pk_column(),
   password : password_column(),
   salt : salt_column(),
   name : name_column(),
@@ -25,7 +27,7 @@ const user = sequelize.define('user', {
 
 //company company model
 const company = sequelize.define('company', {
-  company_adminemail : email_column(),
+  company_adminemail : email_pk_column(),
   company_name : name_column(),
   password : password_column(),
   salt: salt_column()
@@ -35,12 +37,12 @@ const company = sequelize.define('company', {
 
 //creating policy model
 const policy = sequelize.define('policy', {
-  id : id_column(),
+  id : id_pk_column(),
   policywording : text_column(),
   roomrentcap :money_column(),
   suminsured :money_column(),
-  company_adminemail : { type: DataTypes.STRING, validate: {isEmail: true}, references: {model: company ,key: 'company_adminemail'}},
-  exemptions : money_column(),
+  company_adminemail : Object.assign({},email_column(),{references: {model: company ,key: 'company_adminemail'}}),
+  exemptions : text_column(),
   claim_settlement_ratio : {type: DataTypes.DOUBLE,allowNull: false,validate: {isNumeric: true, isFloat: true, min:0, max:100}}
 }, {
   timestamps: false
@@ -48,9 +50,9 @@ const policy = sequelize.define('policy', {
 
 //purchased policies model
 const purchasedpolicy = sequelize.define('purchasedpolicy', {
-  id : id_column(),
-  useremail : { type: DataTypes.STRING, validate: {isEmail: true}, references: {model: user ,key: 'email'} },
-  policyid : {type: DataTypes.INTEGER,allowNull: false,references: {model: policy,key: 'id'}},
+  id : id_pk_column(),
+  useremail : Object.assign({},email_column(),{references: {model: user ,key: 'email'}}),
+  policyid : Object.assign({},id_column(),{references: {model: policy ,key: 'id'}}),
   policyholdername : name_column(),
   policyholderage : money_column(),
   policyage : money_column(),
@@ -63,8 +65,8 @@ const purchasedpolicy = sequelize.define('purchasedpolicy', {
 });
 
 const claim = sequelize.define('claim', {
-  id : id_column(),
-  bondid : { type: DataTypes.INTEGER, references: {model: purchasedpolicy ,key: 'id'} },
+  id : id_pk_column(),
+  bondid : Object.assign({},id_column(),{references: {model: purchasedpolicy ,key: 'id'}}),
   reason : text_column(),
   claim_amount : money_column(),
   hospital_name : name_column(),
@@ -83,4 +85,4 @@ const claim = sequelize.define('claim', {
   console.log("All models were synchronized successfully.");
 })();
 
-module.exports = {user, company, policy, purchasedpolicy,claim};
+module.exports = {user, company, policy, purchasedpolicy, claim};
--- Router/routes.js
+++ Router/routes.js
@@ -5,27 +5,26 @@ const router = express.Router();
 
 router.use(middleware);
 
+router.post('/signup_company',controller.signup_company);
+router.post('/login_company',controller.login_company);
+
+router.post('/createpolicy',controller.authorise_company,controller.createpolicy);
+router.get('/view_policies_of_my_company',controller.authorise_company,controller.view_policies_of_my_company);
+router.get('/view_claims_of_my_company',controller.authorise_company,controller.view_claims_of_my_company);
+
+router.get('/logout_company', controller.authorise_company, controller.logout);
 
 router.post('/signup_user',controller.signup_user);
 router.post('/login_user',controller.login_user);
-router.get('/logout_user', controller.authorise_user, controller.logout);
 
-router.post('/signup_company',controller.signup_company);
-router.post('/login_company',controller.login_company);
-router.get('/logout_company', controller.authorise_company, controller.logout);
-
-router.post('/createpolicy',controller.authorise_company,controller.createpolicy);
-router.post('/buy', controller.authorise_user, controller.buypolicy);
-
-router.get('/viewmypolicies',controller.authorise_user,controller.view_my_policies);
 router.get('/policies', controller.viewpolicies);
+router.post('/buy', controller.authorise_user, controller.buypolicy);
+router.get('/viewmypolicies',controller.authorise_user,controller.view_my_policies);
 
 router.post('/claimpolicy',controller.authorise_user,controller.claim_my_policy);
 router.get('/viewmyclaims',controller.authorise_user,controller.viewmyclaims);
 
-router.get('/view_policies_of_my_company',controller.authorise_company,controller.view_policies_of_my_company);
-router.get('/view_claims_of_my_company',controller.authorise_company,controller.view_claims_of_my_company);
-
+router.get('/logout_user', controller.authorise_user, controller.logout);
 
 router.use('*', controller.page_404);
 
